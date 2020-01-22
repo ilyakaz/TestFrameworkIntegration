59c6d6a84ceb1697438d2f486b56b44e275bb586
Ilya Kazakov <ilya.kazakov@canfieldsci.com>
2020-01-22 16:55:53 -0500

    Updated framework

---
+++ TestSwiftFrameworkHarness/TestSwiftFrameworkHarness/TestSwiftFramework.framework/PrivateHeaders/TestObjCClass.h
@@ -0,0 +1,15 @@
+//
+//  TestObjCClass.h
+//  TestSwiftFramework
+//
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestObjCClass : NSObject
+-(void)printTestMessage;
+@end
+
+NS_ASSUME_NONNULL_END
